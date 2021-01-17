30e2a4a050bcad5a2878b53d5768d80781be0ac6
adlymagdy <1126032+adlymagdy@users.noreply.github.com>
2021-01-17 22:42:41 +0200

    Create store-todos.js

---
+++ TodoList/Arrays/store-todos.js
@@ -0,0 +1,8 @@
+let todos = ["item 1", "item 2", "item 3"]
+console.log("My todos:", todos)
+todos.push("item 4")
+console.log(todos)
+todos[0] = "item 1 updated"
+console.log(todos)
+todos.splice(1,2)
+console.log(todos)
